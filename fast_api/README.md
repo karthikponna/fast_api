876a1f18ad50e7045192c75da794557b420aefc8
karthikponna <karthikponna963@gmail.com>
2025-07-07 21:04:21 +0530

    add initial fast api files

---
+++ fast_api/README.md
@@ -0,0 +1,10 @@
+# Introduction to FAST API
+
+## HTTP Request Methods
+
+- GET: Read resource
+- POST: Create resource
+- PUT: Update/Replace resource
+- DELETE: Delete resource
+
+# write about pydantic - BaseModel, Field
---
+++ fast_api/books.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from uuid import UUID
+
+
+app = FastAPI()
+
+class Book(BaseModel):
+    id: UUID
+    title: str = Field(min_length = 1)
+    author: str = Field(min_length = 1, max_length = 100)
+    description: str = Field(min_length = 1, max_length = 100)
+    rating: int = Field(gt = -1, lt = 101)
+
+BOOKS = []
+
+# To create a simple get request 
+@app.get("/")
+def read_api():
+    return BOOKS
+
+@app.post("/")
+def create_book(book:Book):
+    BOOKS.append(book)
+    return book
+
+# To create a simple get request with placeholder where user can give input
+# @app.get("/{name}")
+# def read_name(name:str):
+#     return {"Hey": {name}}
