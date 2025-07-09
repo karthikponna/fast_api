0f1613d2d4e6f4904fa57e259e846483c1571c4e
karthikponna <karthikponna963@gmail.com>
2025-07-09 20:47:44 +0530

    final updates to the books file

--- fast_api/books.py
+++ fast_api/books.py
@@ -24,8 +24,34 @@ def create_book(book:Book):
     BOOKS.append(book)
     return book
 
+@app.put("/{book_id}")
+def update_book(book_id:UUID, book: Book):
+    counter = 0
 
+    for x in BOOKS:
+        counter+=1
+        if x.id == book_id:
+            BOOKS[counter - 1] = book
+            return BOOKS[counter - 1]
+    
+    raise HTTPException(
+        status_code= 404,
+        detail = f"ID {book_id}: Does not exist"
+    )
 
+@app.delete("/{book_id}")
+def delete_book(book_id:UUID):
+    counter = 0
+
+    for x in BOOKS:
+        counter+=1
+        if x.id == book_id:
+            del BOOKS[counter - 1]
+            return f"ID {book_id} deleted"
+    raise HTTPException(
+        status_code = 404, 
+        detail = f"ID {book_id}: Does not exist"
+    )
 
 
 
